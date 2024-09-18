c18a3d9b4f59225241d905c0c4a6028fac2eef3d
Eby Joshy Kavungal <ebyjkavungal@gmail.com>
2024-09-19 02:19:17 +0530

    second commit

--- paging.c
+++ paging.c
@@ -1,11 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+struct process{
+    int id;
+    int size;
+    int nofr;
+    int fno[10];
+}p[10];
 
-struct{
+int main(){
+            int msize,fsize,totfr,remfr,ch;
+            int top=0;
+            printf("Enter memory size: \n");
+            scanf("%d",&msize);
+            printf("enter frame size:\n");
+            scanf("%d",&fsize);
+            totfr= msize/fsize;
+            remfr=totfr;
 
-};
+            srand(time(NULL));
 
-void main(){
+            printf("Select choice\n1.insert process\n2.delete process.\n3.exit.\n");
+
+            while(1){
+                printf("\nEnter choice:\n");
+                scanf("%d",&ch);
+
+                switch(ch){
+                    case 1:{
+                        if(remfr==0){
+                            printf("memory is full!\n");
+                            break;
+                        }
+                        printf("enter process ID:\n");
+                        scanf("%d",&p[top].id);
+                        printf("enter process size:\n");
+                        scanf("%d",&p[top].size);
+                        
+                    }
+                }
+            }
 
 
+
+
+ return 0;
 }
