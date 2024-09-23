373d8f2cd7becd28a10d983379ad138ab1f8438d
Eby Joshy Kavungal <ebyjkavungal@gmail.com>
2024-09-24 01:17:10 +0530

    final commit

--- paging.c
+++ paging.c
@@ -1,143 +1,132 @@
+
 // #include <stdio.h>
 // #include <stdlib.h>
 // #include <time.h>
-// struct process{
+
+// struct process {
 //     int id;
 //     int size;
 //     int nofr;
 //     int fno[10];
-// }p[10];
+// } p[10];
 
-// int main(){
-//             int msize,fsize,totfr,remfr,ch;
-//             int top=0;
-//             int infrag,pid;
-//             printf("Enter memory size(in kb): \n");
-//             scanf("%d",&msize);
-//             printf("enter frame size:\n");
-//             scanf("%d",&fsize);
-//             totfr= msize/fsize;
-//             printf("total no of frames available: %d\n",totfr);
-//             remfr=totfr;
+// int main() {
+//     int msize, fsize, totfr, remfr, ch;
+//     int top = 0;
+//     int infrag, pid;
 
-//             srand(time(NULL));
+//     printf("Enter memory size(in kb): \n");
+//     scanf("%d", &msize);
+//     printf("Enter frame size:\n");
+//     scanf("%d", &fsize);
+//     totfr = msize / fsize;
+//     printf("Total no of frames available: %d\n", totfr);
+//     remfr = totfr;
 
-//             printf("\nSelect choice\n1.insert process\n2.delete process.\n3.exit.\n");
+//     srand(time(NULL));
 
-//             while(1){
-//                 printf("\nEnter choice:\n");
-//                 scanf("%d",&ch);
+//     printf("\nSelect choice\n1. Insert process\n2. Delete process.\n3. Exit.\n");
 
-//                 switch(ch){
-//                     case 1:{
-//                         if(remfr==0){
-//                             printf("memory is full!\n");
-//                             break;
-//                         }
-//                         printf("enter process ID:\n");
-//                         scanf("%d",&p[top].id);
-//                         printf("enter process size:\n");
-//                         scanf("%d",&p[top].size);
-                        
-//                         p[top].nofr=(p[top].size+fsize-1)/fsize;
-//                         if(p[top].nofr>remfr){
-//                             printf("Memory overflow!\n choose a smaller size!\n");
-//                         }
-//                         remfr -= p[top].nofr;
+//     while (1) {
+//         printf("\nEnter choice:\n");
+//         scanf("%d", &ch);
 
-//                         for(int i=0;i<p[top].nofr;i++){
-//                             p[top].fno[i] =rand() % msize +1;
-//                         }
-//                         top++;
+//         switch (ch) {
+//             case 1: {
+//                 if (remfr == 0) {
+//                     printf("Memory is full!\n");
+//                     break;
+//                 }
+//                 printf("Enter process ID:\n");
+//                 scanf("%d", &p[top].id);
+//                 printf("Enter process size:\n");
+//                 scanf("%d", &p[top].size);
 
-//                         printf("\n Process SIze Frames\n");
-//                         infrag=0;
-//                         for(int i=0;i<top;i++){
-//                             printf("%7d%7d", p[i].id,p[i].size);
-//                             for(int j=0;j<p[i].nofr;j++){
-//                                 printf("%2d",p[i].fno[j]);
-//                                 printf(" ");
-//                             }
-//                             int last_frame_unused=fsize -(p[i].size%fsize);
-//                             if(last_frame_unused!=fsize)
-//                              {
-//                                 infrag += last_frame_unused;
-//                              }
-//                              printf("\n");
+//                 p[top].nofr = (p[top].size + fsize - 1) / fsize;  // Calculate frames required
+//                 if (p[top].nofr > remfr) {
+//                     printf("Memory overflow! Choose a smaller size!\n");
+//                     break;
+//                 }
+//                 remfr -= p[top].nofr;
 
+//                 for (int i = 0; i < p[top].nofr; i++) {
+//                     p[top].fno[i] = rand() % totfr + 1;  // Limit to available frames
+//                 }
+//                 top++;
 
-//                         }
-//                         printf("\nRemaining Frames: %d\nInternal fragmentation: %d bytes\n",remfr,infrag);
-//                         break;
+//                 printf("\nProcess ID    Size    Frames\n");
+//                 infrag = 0;
+//                 for (int i = 0; i < top; i++) {
+//                     printf("%7d%7d", p[i].id, p[i].size);
+//                     for (int j = 0; j < p[i].nofr; j++) {
+//                         printf("%4d ", p[i].fno[j]);
 //                     }
-//                     case 2:{
-//                         int d;
-//                         printf("Enter process ID:\n");
-//                         scanf("%d",&pid);
-//                         for(d=0;d< top;d++){
-//                             if(p[d].id == pid){
-//                                 break;
-//                             }
-//                         }
-//                         if(d==top){
-//                             printf("Process doesn't exist\n");
-//                             break;
-//                         }
-//                         remfr += p[d].nofr;
-//                         for(int i=d; i<top-1;i++){
-//                             p[i]=p[i+1];
-
-//                         }
-//                         top --;
-//                         //printf("\n process size frames\n");
-//                         printf("process deleted");
-//                         infrag=0;
-//                         for(int i=0;i<top;i++){
-//                             printf("%4d%3d",p[i].id,p[i].size);
-//                             for(int j=0;j<p[i].nofr;j++){
-//                                 printf("%2d",p[i].fno[j]);
-
-//                             }
-//                             int last_frame_unused =fsize-(p[i].size% fsize);
-//                             if(last_frame_unused !=fsize){
-//                                 infrag += last_frame_unused;
-
-//                             }
-//                             printf("\n");
-//                         } 
-//                         printf("\nRemaining frames: %d\nInternal fragmentation: %d bytes\n",remfr,infrag);
+//                     int last_frame_unused = fsize - (p[i].size % fsize);
+//                     infrag += last_frame_unused ;  // Internal fragmentation
+//                     printf("\n");
+//                 }
+//                 printf("\nRemaining Frames: %d\nInternal fragmentation: %d bytes\n", remfr, infrag);
+//                 break;
+//             }
+//             case 2: {
+//                 printf("Enter process ID:\n");
+//                 scanf("%d", &pid);
+//                 int d;
+//                 for (d = 0; d < top; d++) {
+//                     if (p[d].id == pid) {
 //                         break;
-
-//                     }
-//                     case 3:
-//                     {
-//                         printf("Exitting the program!\n");
-//                         printf("\n");
-//                         exit(0);
 //                     }
 //                 }
+//                 if (d == top) {
+//                     printf("Process doesn't exist\n");
+//                     break;
+//                 }
+//                 remfr += p[d].nofr;
+//                 for (int i = d; i < top - 1; i++) {
+//                     p[i] = p[i + 1];
+//                 }
+//                 top--;
+
+//                 printf("\nProcess ID    Size    Frames\n");
+//                 infrag = 0;
+//                 for (int i = 0; i < top; i++) {
+//                     printf("%7d%7d", p[i].id, p[i].size);
+//                     for (int j = 0; j < p[i].nofr; j++) {
+//                         printf("%4d ", p[i].fno[j]);
+//                     }
+//                     int last_frame_unused = fsize - (p[i].size % fsize);
+//                     infrag += last_frame_unused % fsize;
+//                     printf("\n");
+//                 }
+//                 printf("\nRemaining frames: %d\nInternal fragmentation: %d bytes\n", remfr, infrag);
+//                 break;
 //             }
+//             case 3: {
+//                 printf("Exiting the program!\n");
+//                 exit(0);
+//             }
+//         }
+//     }
 
-
-
-
-//  return 0;
+//     return 0;
 // }
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 struct process {
-    int id;
-    int size;
-    int nofr;
-    int fno[10];
+    int id;        
+    int size;      
+    int nofr;      
+    int fno[10];   
 } p[10];
 
 int main() {
     int msize, fsize, totfr, remfr, ch;
     int top = 0;
     int infrag, pid;
+    int total_infrag = 0;
 
     printf("Enter memory size(in kb): \n");
     scanf("%d", &msize);
@@ -149,85 +138,111 @@ int main() {
 
     srand(time(NULL));
 
-    printf("\nSelect choice\n1. Insert process\n2. Delete process.\n3. Exit.\n");
-
     while (1) {
-        printf("\nEnter choice:\n");
+        printf("\nMenu\n1. Insert\n2. Delete\n3. Display PCB\n4. Exit\nEnter choice: ");
         scanf("%d", &ch);
 
         switch (ch) {
             case 1: {
                 if (remfr == 0) {
-                    printf("Memory is full!\n");
+                    printf("Memory full!\n");
                     break;
                 }
-                printf("Enter process ID:\n");
+                
+                printf("Enter process ID :\n");
                 scanf("%d", &p[top].id);
                 printf("Enter process size:\n");
                 scanf("%d", &p[top].size);
 
-                p[top].nofr = (p[top].size + fsize - 1) / fsize;  // Calculate frames required
+                p[top].nofr = (p[top].size + fsize - 1) / fsize;  // round-off considered
                 if (p[top].nofr > remfr) {
-                    printf("Memory overflow! Choose a smaller size!\n");
+                    printf("Memory full or insufficient space for this process!\n");
                     break;
                 }
                 remfr -= p[top].nofr;
 
                 for (int i = 0; i < p[top].nofr; i++) {
-                    p[top].fno[i] = rand() % totfr + 1;  // Limit to available frames
+                    p[top].fno[i] = rand() % totfr + 1;  //limited by total frame
                 }
-                top++;
 
-                printf("\nProcess ID    Size    Frames\n");
-                infrag = 0;
-                for (int i = 0; i < top; i++) {
-                    printf("%7d%7d", p[i].id, p[i].size);
-                    for (int j = 0; j < p[i].nofr; j++) {
-                        printf("%4d ", p[i].fno[j]);
-                    }
-                    int last_frame_unused = fsize - (p[i].size % fsize);
-                    infrag += last_frame_unused ;  // Internal fragmentation
-                    printf("\n");
+                int last_frame_unused = fsize - (p[top].size % fsize);
+                if (p[top].size % fsize != 0) {
+                    infrag = last_frame_unused;
+                } else {
+                    infrag = 0;
                 }
-                printf("\nRemaining Frames: %d\nInternal fragmentation: %d bytes\n", remfr, infrag);
+                total_infrag += infrag;
+
+                printf("Process allocated\n");
+                printf("Internal Fragmentation: %d\n", total_infrag);
+                top++;
                 break;
             }
             case 2: {
-                printf("Enter process ID:\n");
+                if (top == 0) {
+                    printf("No processes to delete!\n");
+                    break;
+                }
+
+                printf("Enter process ID (integer):\n");
                 scanf("%d", &pid);
+
                 int d;
                 for (d = 0; d < top; d++) {
                     if (p[d].id == pid) {
                         break;
                     }
                 }
+
                 if (d == top) {
                     printf("Process doesn't exist\n");
                     break;
                 }
+
                 remfr += p[d].nofr;
+
+                int last_frame_unused = fsize - (p[d].size % fsize);
+                if (p[d].size % fsize != 0) {
+                    total_infrag -= last_frame_unused;
+                }
+
                 for (int i = d; i < top - 1; i++) {
-                    p[i] = p[i + 1];
+                    p[i] = p[i + 1];  
                 }
                 top--;
 
-                printf("\nProcess ID    Size    Frames\n");
-                infrag = 0;
-                for (int i = 0; i < top; i++) {
-                    printf("%7d%7d", p[i].id, p[i].size);
-                    for (int j = 0; j < p[i].nofr; j++) {
-                        printf("%4d ", p[i].fno[j]);
-                    }
-                    int last_frame_unused = fsize - (p[i].size % fsize);
-                    infrag += last_frame_unused % fsize;
-                    printf("\n");
-                }
-                printf("\nRemaining frames: %d\nInternal fragmentation: %d bytes\n", remfr, infrag);
+                printf("Process deleted\n");
+                printf("Internal Fragmentation: %d\n", total_infrag);
                 break;
             }
             case 3: {
+                if (top == 0) {
+                    printf("No processes to display!\n");
+                    break;
+                }
+
+                printf("\nProcess Control Block\n");
+                printf("-------------------------------------------------\n");
+                printf("Process ID  Process Size   Frames Allocated\n");
+                printf("-------------------------------------------------\n");
+                for (int i = 0; i < top; i++) {
+                    printf("%10d%14d", p[i].id, p[i].size);
+                    for (int j = 0; j < p[i].nofr; j++) {
+                        printf("%4d ", p[i].fno[j]);
+                    }
+                    printf("\n");
+                }
+                printf("-------------------------------------------------\n");
+                printf("Total Internal Fragmentation: %d\n", total_infrag);
+                break;
+            }
+            case 4: {
                 printf("Exiting the program!\n");
+                printf("\n");
                 exit(0);
+            }
+            default: {
+                printf("Invalid choice, please enter again!\n");
             }
         }
     }
