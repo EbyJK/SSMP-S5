f6550e7b95361a273ff081bfa994fb5566a0df67
Eby Joshy Kavungal <ebyjkavungal@gmail.com>
2024-10-07 21:27:07 +0530

    initial commit

---
+++ pass1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+void main(){
+        FILE *input_file,*intermediate_file,*symbtab,*optab, *program_size;
+        
+
+
+}
