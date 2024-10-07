1619d377b1cc302005192c2d6eb65895c8e3bf86
Eby Joshy Kavungal <ebyjkavungal@gmail.com>
2024-10-07 21:34:51 +0530

    initialcommit1

--- pass1.c
+++ pass1.c
@@ -4,7 +4,14 @@
 #include<time.h>
 void main(){
         FILE *input_file,*intermediate_file,*symbtab,*optab, *program_size;
-        
+        input_file=fopen("input.txt","r");
+        optab=fopen("optab.txt","r");
+        symbtab=fopen("symtab.txt","w+");
+        intermediate_file=fopen("intermediatefile.txt","w");
+        program_size=fopen("size.txt","w");
+            
+
+
 
 
 }
