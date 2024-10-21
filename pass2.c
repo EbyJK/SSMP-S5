3c2db4605358890d1f93a05f92f6582984584f3e
Eby Joshy Kavungal <ebyjkavungal@gmail.com>
2024-10-21 22:00:02 +0530

    updated code

--- pass2.c
+++ pass2.c
@@ -1,84 +1,207 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<time.h>
 #include<math.h>
-char h[5];
-void int_to_hex(int num,char hex[]);
-void main(){
-    FILE *objectprogram,*intermediate_file,*symbtab,*optab,*program_size;
-    objectprogram=fopen("objectprogram.txt","r");
-    optab=fopen("optab.txt","r");
-    symbtab=fopen("symbtab.txt","r");
-    intermediate_file=fopen("intermediatefile.txt","w");
-    program_size=fopen("size.txt","w");
 
-    char str1[100],str2[100],str3[100],s1[100],s2[100],start[100],temp[100];
-    char str[100]="";
-    int need=0,size;
-    strcat(str,"H");
-    fscanf(intermediate_file,"%s %s %s",str1,str2,str3);
-    strcat(str,str1);
-    strcpy(start,str3);
-    if(strlen(start)<6){
-        need=6-strlen(start);
-        for(int i=0;i<need;i++){
-            strcat(str,"0");
+char h[7];  // Adjusted to accommodate 6 digits in hex
+
+void int_to_hex(int num, char hex[]);
+
+void main() {
+    FILE *objectprogram, *intermediate_file, *symbtab, *optab, *program_size;
+    objectprogram = fopen("objectprogram.txt", "w");
+    optab = fopen("optab.txt", "r");
+    symbtab = fopen("symbtab.txt", "r");
+    intermediate_file = fopen("intermediatefile.txt", "r");  // Open for reading
+    program_size = fopen("size.txt", "r");  // Open for reading
+
+    if (!objectprogram || !intermediate_file || !optab || !symbtab || !program_size) {
+        printf("Error opening files\n");
+        exit(1);
+    }
+
+    char str1[100], str2[100], str3[100], s1[100], s2[100], start[100], temp[100], str4[100];
+    char str[100] = "";
+    int need = 0, size;
+   
+    // Construct H record
+    strcat(str, "H");
+    fscanf(intermediate_file, "%s %s %s", str1, str2, str3);
+    strcat(str, str1);
+    strcpy(start, str3);
+
+    // Pad start address to 6 characters
+    if (strlen(start) < 6) {
+        need = 6 - strlen(start);
+        for (int i = 0; i < need; i++) {
+            strcat(str, "0");
         }
-        strcat(str,start);
+        strcat(str, start);
+    } else if (strlen(start) == 6) {
+        strcat(str, start);
     }
-    else if(strlen(start)==6){
-        strcat(str,start);
 
-    }
-    fscanf(program_size,"%d",&size);
-    int_to_hex(size,h);
-    strcpy(temp,"");
-    if(strlen(h)<6){
-        need=6-strlen(h);
-        for(int i=0;i<need;i++){
-            strcat(str,"0");
+    // Read the program size and convert to hex
+    fscanf(program_size, "%d", &size);
+    int_to_hex(size, h);
+
+    // Pad program size to 6 characters
+    if (strlen(h) < 6) {
+        need = 6 - strlen(h);
+        for (int i = 0; i < need; i++) {
+            strcat(str, "0");
         }
-        strcat(str,h);
+        strcat(str, h);
+    } else {
+        strcat(str, h);
     }
-    else if(strlen(h)==6){      
-        strcat(str,h);
 
-    }
-    fprintf(objectprogram,"%s\n",str);
-    strcpy(str,"");
-    strcat(str,"T");
-    if(strlen(start)<6){
-        need=6-strlen(start);
-        for(int i=0;i<need;i++){
-            strcat(str,"0");
+    // Write the H record to the object program
+    fprintf(objectprogram, "%s\n", str);
+    strcpy(str, "");
+    strcat(str, "T");
+
+    // Pad the start address to 6 characters again for T record
+    if (strlen(start) < 6) {
+        need = 6 - strlen(start);
+        for (int i = 0; i < need; i++) {
+            strcat(str, "0");
         }
-        strcat(str,start);
+        strcat(str, start);
+    } else {
+        strcat(str, start);
     }
-    else if(strlen(start)==6){
-        strcat(str,start);
+
+    // Set initial length for T record (arbitrary for now)
+    int length = 9;
+    sprintf(h, "%02X", length);
+    strcat(str, h);
+
+    // Process instructions from intermediate file
+    fscanf(intermediate_file, "%x %s %s %s", &size, str2, str3, str4);
+    while (strcmp(str2, "ND") != 0) {
+        if (strcmp(str3, "RESB") == 0 || strcmp(str3, "RESW") == 0) {
+            fscanf(intermediate_file, "%x %s %s %s", &size, str2, str3, str4);
+            continue;
+        }
+
+        // Check if T record length exceeds allowed size
+        if ((strlen(str) + 6) > ((length * 2) + 9)) {
+            fprintf(objectprogram, "%s\n", str);
+            strcpy(str, "");
+            strcat(str, "T");
+
+            // Adjust size and write length in hex
+            sprintf(h, "%X", size);
+            if (strlen(h) < 6) {
+                need = 6 - strlen(h);
+                for (int i = 0; i < need; i++) {
+                    strcat(str, "0");
+                }
+            }
+            strcat(str, h);
+            sprintf(h, "%02X", length);
+            strcat(str, h);
+        }
+
+        char hex[3] = {0};
+
+        // Process BYTE and WORD
+        if (strcmp(str3, "BYTE") == 0 || strcmp(str3, "WORD") == 0) {
+            if (strcmp(str3, "BYTE") == 0) {
+                strcpy(temp, "");
+                for (int i = 2; i < strlen(str4) - 1; i++) {
+                    sprintf(hex, "%02X", (unsigned char)str4[i]);
+                    strcat(temp, hex);
+                }
+            } else {
+                size = atoi(str4);
+                sprintf(h, "%X", size);
+                strcpy(temp, h);
+            }
+           
+            // Check if adding the temp data exceeds T record length
+            if ((strlen(str) + 6 + (strlen(temp) / 2)) > ((length * 2) + 9)) {
+                fprintf(objectprogram, "%s\n", str);
+                strcpy(str, "");
+                strcat(str, "T");
+                sprintf(h, "%X", size);
+                if (strlen(h) < 6) {
+                    need = 6 - strlen(h);
+                    for (int i = 0; i < need; i++) {
+                        strcat(str, "0");
+                    }
+                }
+                strcat(str, h);
+            }
+
+            strcat(str, temp);
+        }
+
+        // Process opcode
+        int flag = 0;
+        fscanf(optab, "%s %s", s1, s2);
+        if (str3[0] == '+') {
+            flag = 1;
+        }
+
+        while (1) {
+            if (strcmp(s1, str3) == 0 && flag == 0) {
+                break;
+            }
+            if (strcmp(s1, str3 + 1) == 0 && flag == 1) {
+                break;
+            }
+            fscanf(optab, "%s %s", s1, s2);
+        }
+
+        strcat(str, s2);
+        rewind(optab);
+
+        // Read the next instruction
+        fscanf(intermediate_file, "%X %s %s %s", &size, str2, str3, str4);
     }
-    //
 
+    // Write the final T record
+    fprintf(objectprogram, "%s\n", str);
 
+    // Write the E record
+    strcpy(str, "E");
+    if (strlen(start) < 6) {
+        need = 6 - strlen(start);
+        for (int i = 0; i < need; i++) {
+            strcat(str, "0");
+        }
+        strcat(str, start);
+    } else {
+        strcat(str, start);
+    }
+    fprintf(objectprogram, "%s\n", str);
+
+    // Close the files
+    fclose(objectprogram);
+    fclose(optab);
+    fclose(symbtab);
+    fclose(intermediate_file);
+    fclose(program_size);
+
+    printf("Pass 2 completed!\n");
 }
-void int_to_hex(int num,char hex[]){
-for (int j=0;j<4;j++){
-hex[j]='0';
-}
-hex[4]='\0';
-int i=3;
-while(num!=0){
-int remainder=num%16;
-if(remainder<10){
-hex[i]=remainder+48;
 
-}else{
-
-hex[i]=(remainder-10)+65;
-}
-num/=16;
-i--;
-
-}
+void int_to_hex(int num, char hex[]) {
+    for (int j = 0; j < 6; j++) {  // Ensure we have enough room for 6 hex digits
+        hex[j] = '0';
+    }
+    hex[6] = '\0';
+    int i = 5;
+    while (num != 0) {
+        int remainder = num % 16;
+        if (remainder < 10) {
+            hex[i] = remainder + 48;
+        } else {
+            hex[i] = (remainder - 10) + 65;
+        }
+        num /= 16;
+        i--;
+    }
 }
